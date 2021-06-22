fff043494e6d63d0542c7ff57fe46fb03aaefe55
moonlight025 <tw2016905@gmail.com>
2021-06-23 01:36:09 +0800

    second commit

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0f449!la#omlt2ek#9g=c2l+%79qaj-*-*al&_^$o7b11)*9bj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','finalweb2021.herokuapp.com']
 
 
 # Application definition
